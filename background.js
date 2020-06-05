6cffdb5d4e3cf2e0e5f1e26d0ed5d985f5f93aa6
DEViA <devia01@mail.com>
2020-06-05 20:22:20 +0900

    Fix bug that didn't support CORS iframe video

--- background.js
+++ background.js
@@ -1,18 +1,16 @@
 chrome.browserAction.onClicked.addListener(function () {
+  let playbackRate = prompt("동영상 배속 설정", "1.0");
+  if (!+playbackRate) {
+    alert("올바른 숫자값을 입력해 주세요");
+    playbackRate = "1.0";
+  }
 
-    let playbackRate = prompt('동영상 배속 설정', '1.0');
-    if (!+playbackRate) {
-        alert("올바른 숫자값을 입력해 주세요");
-        playbackRate = "1.0"
-    }
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            null,
-            {code:
-            `document.querySelectorAll('video').forEach(v => v.playbackRate = "${playbackRate}");
-            document.querySelectorAll('iframe').forEach(item => item.contentWindow.document.body.querySelectorAll('video').forEach(v => v.playbackRate = "${playbackRate}"));`
-        });
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(null, {
+      code: `
+        document.querySelectorAll('video').forEach(v => v.playbackRate = ${playbackRate});
+        `,
+      allFrames: true
     });
-
+  });
 });
